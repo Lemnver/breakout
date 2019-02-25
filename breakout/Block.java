3a7532793092999ea65b17ee736ea42fa1f9e3a3
lemnver <lemnver@github.com>
2019-02-25 19:57:25 +0100

    logic for blocks

--- breakout/Block.java
+++ breakout/Block.java
@@ -4,7 +4,20 @@ public class Block extends Obstacle {
     /*  class for blocks that Ball collides with
      *
      */
-
-    public Block(){
+    private int _lives;
+    public Block(Coordinates coordinates, int height, int width, int hitpoints){
+        super(coordinates, height, width);
+        _lives = hitpoints;
+    }
+    public void collision(Rectangle opponent){
+        hit();
+    }
+    public void hit(){
+        if (_lives > 0) {
+            _lives -= 1;
+        }
+    }
+    public int getLives(){
+        return _lives;
     }
 }
